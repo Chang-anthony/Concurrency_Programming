7e41d9d4b61e83575e8d8a3db75699acf9a0a25a
TonyChang <a23011257@gmail.com>
2024-06-19 13:33:19 +0800

    first create concurrency programming env

---
+++ src/source.cmake
@@ -0,0 +1,16 @@
+set(LIB_NAME "TREASUREMAP_INCLUDE")
+set(LIB_PATH ${CMAKE_CURRENT_LIST_DIR})
+
+set(__SRCS)
+
+# include(${LIB_PATH}/Mapobject/mapobject.cmake)
+# list(APPEND __SRCS ${LIB_SRCS})
+
+# # message("${LIB_PATH}")
+# include(${LIB_PATH}/Game/game.cmake)
+# list(APPEND __SRCS ${LIB_SRCS})
+
+# include(${LIB_PATH}/Role/role.cmake)
+# list(APPEND __SRCS ${LIB_SRCS})
+
+set(LIB_SRCS ${__SRCS})
